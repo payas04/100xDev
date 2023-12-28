340055c6468dff63104459ef8eec64035b85dd4f
payas04 <payaspatel@gmail.com>
2023-12-28 20:29:45 +0530

    Assignments updated

---
+++ trial.js
@@ -0,0 +1,11 @@
+function Square(n){
+    return n * n;
+}
+
+function SumofSquares(a,b){
+    return Square(a) + Square(b);
+   
+}
+
+console.log("The square of 5 is: " + Square(10)); 
+console.log("The sum of the squares of 3 and 4 is: " + SumofSquares(5,4));
